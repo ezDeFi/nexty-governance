7dbb07643cb9f63ba4b1c5aaa149a144b607f810
abc def <noluckjustfair@gmail.com>
2018-11-08 17:54:54 +0700

    fix prettier

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,25 +2,17 @@ var NTFToken = artifacts.require('./token/ERC20/NTFToken.sol');
 var NextyManager = artifacts.require('./governance/NextyManager.sol');
 
 module.exports = async function(deployer) {
-    let NTFTokenInst, NextyManagerInst;
+  let NTFTokenInst, NextyManagerInst;
 
-    await Promise.all([
+  await Promise.all([deployer.deploy(NTFToken), deployer.deploy(NextyManager)]);
 
-        deployer.deploy(NTFToken),
-        deployer.deploy(NextyManager)
-    ])
+  instances = await Promise.all([NTFToken.deployed(), NextyManager.deployed()]);
 
-    instances = await Promise.all([
-        NTFToken.deployed(),
-        NextyManager.deployed()
-    ])
+  NTFTokenInst = instances[0];
+  NextyManagerInst = instances[1];
 
-    NTFTokenInst = instances[0];
-    NextyManagerInst = instances[1];
-
-    results = await Promise.all([
-        NTFTokenInst.initialize('0x6f53c8502bb884775e422c7c34be681554cee2ba'),
-        NextyManagerInst.initialize(NTFToken.address)
-    ]);
-
+  results = await Promise.all([
+    NTFTokenInst.initialize('0x6f53c8502bb884775e422c7c34be681554cee2ba'),
+    NextyManagerInst.initialize(NTFToken.address),
+  ]);
 };
--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,10 @@
 import NTFToken from './../build/contracts/NTFToken.json';
 import NextyManager from './../build/contracts/NextyManager.json';
 export const USER_ROLE = {
-    MEMBER: 'MEMBER',
-    LEADER: 'LEADER',
-    ADMIN: 'ADMIN',
-    COUNCIL: 'COUNCIL'
+  MEMBER: 'MEMBER',
+  LEADER: 'LEADER',
+  ADMIN: 'ADMIN',
+  COUNCIL: 'COUNCIL',
 };
 
 const CONTRACT_ABI_NTFToken = NTFToken.abi;
@@ -14,98 +14,98 @@ const CONTRACT_ABI_NextyManager = NextyManager.abi;
 const CONTRACT_ADDRESS_NextyManager = NextyManager.networks['66666'].address;
 
 export const WEB3 = {
-    // HTTP: 'http://125.212.250.61:11111', // testnet
-    HTTP: 'http://13.228.68.50:8545', // mainnet
-    PAGE: {
-        'NTFToken': {
-            ABI: CONTRACT_ABI_NTFToken,
-            ADDRESS: CONTRACT_ADDRESS_NTFToken
-        },
-        'NextyManager': {
-            ABI: CONTRACT_ABI_NextyManager,
-            ADDRESS: CONTRACT_ADDRESS_NextyManager
-        }
-    }
-}
+  // HTTP: 'http://125.212.250.61:11111', // testnet
+  HTTP: 'http://13.228.68.50:8545', // mainnet
+  PAGE: {
+    NTFToken: {
+      ABI: CONTRACT_ABI_NTFToken,
+      ADDRESS: CONTRACT_ADDRESS_NTFToken,
+    },
+    NextyManager: {
+      ABI: CONTRACT_ABI_NextyManager,
+      ADDRESS: CONTRACT_ADDRESS_NextyManager,
+    },
+  },
+};
 
 // To change WEB3 ABI ADDRESS
 
 export const USER_LANGUAGE = {
-    en: 'en',
-    zh: 'zh'
-}
+  en: 'en',
+  zh: 'zh',
+};
 
 export const TEAM_ROLE = {
-    MEMBER: 'MEMBER',
-    OWNER: 'OWNER'
+  MEMBER: 'MEMBER',
+  OWNER: 'OWNER',
 };
 
 export const TASK_CATEGORY = {
-    DEVELOPER: 'DEVELOPER',
-    SOCIAL: 'SOCIAL',
-    LEADER: 'LEADER'
-}
+  DEVELOPER: 'DEVELOPER',
+  SOCIAL: 'SOCIAL',
+  LEADER: 'LEADER',
+};
 
 export const TASK_TYPE = {
-    TASK: 'TASK',
-    SUB_TASK: 'SUB_TASK',
-    PROJECT: 'PROJECT',
-    EVENT: 'EVENT'
-}
+  TASK: 'TASK',
+  SUB_TASK: 'SUB_TASK',
+  PROJECT: 'PROJECT',
+  EVENT: 'EVENT',
+};
 
 export const TASK_STATUS = {
-    PROPOSAL: 'PROPOSAL',
-    CREATED: 'CREATED',
-    APPROVED: 'APPROVED',
-    PENDING: 'PENDING',
-    SUCCESS: 'SUCCESS',
-    CANCELED: 'CANCELED',
-    EXPIRED: 'EXPIRED'
-}
+  PROPOSAL: 'PROPOSAL',
+  CREATED: 'CREATED',
+  APPROVED: 'APPROVED',
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  CANCELED: 'CANCELED',
+  EXPIRED: 'EXPIRED',
+};
 
 export const TASK_CANDIDATE_TYPE = {
-    USER: 'USER',
-    TEAM: 'TEAM'
-}
+  USER: 'USER',
+  TEAM: 'TEAM',
+};
 
 export const TASK_CANDIDATE_STATUS = {
-    // NOT_REQUIRED: 'NOT_REQUIRED',
-    PENDING: 'PENDING',
-    APPROVED: 'APPROVED',
-    REJECTED: 'REJECTED'
-}
+  // NOT_REQUIRED: 'NOT_REQUIRED',
+  PENDING: 'PENDING',
+  APPROVED: 'APPROVED',
+  REJECTED: 'REJECTED',
+};
 
 export const COMMUNITY_TYPE = {
-    COUNTRY: 'COUNTRY',
-    STATE: 'STATE',
-    CITY: 'CITY',
-    REGION: 'REGION',
-    SCHOOL: 'SCHOOL'
-}
+  COUNTRY: 'COUNTRY',
+  STATE: 'STATE',
+  CITY: 'CITY',
+  REGION: 'REGION',
+  SCHOOL: 'SCHOOL',
+};
 
 export const TRANS_STATUS = {
-    PENDING: 'PENDING',
-    CANCELED: 'CANCELED',
-    FAILED: 'FAILED',
-    SUCCESSFUL: 'SUCCESSFUL'
-}
+  PENDING: 'PENDING',
+  CANCELED: 'CANCELED',
+  FAILED: 'FAILED',
+  SUCCESSFUL: 'SUCCESSFUL',
+};
 
 export const ISSUE_CATEGORY = {
-    BUG: 'BUG',
-    SECURITY: 'SECURITY',
-    SUGGESTION: 'SUGGESTION',
-    OTHER: 'OTHER'
-}
+  BUG: 'BUG',
+  SECURITY: 'SECURITY',
+  SUGGESTION: 'SUGGESTION',
+  OTHER: 'OTHER',
+};
 
 export const CONTRIB_CATEGORY = {
-    BLOG: 'BLOG',
-    VIDEO: 'VIDEO',
-    PODCAST: 'PODCAST',
-    OTHER: 'OTHER'
-}
+  BLOG: 'BLOG',
+  VIDEO: 'VIDEO',
+  PODCAST: 'PODCAST',
+  OTHER: 'OTHER',
+};
 
 export const DEFAULT_IMAGE = {
-    TASK: '/assets/images/task_thumbs/12.jpg'
+  TASK: '/assets/images/task_thumbs/12.jpg',
 };
 
 export const MIN_VALUE_DEPOSIT = 500000;
