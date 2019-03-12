6ced0c81f757fde23e39f16d30ddbf903e2a59de
Zergity <zergity@gmail.com>
2019-03-12 14:16:48 +0700

    Improve UI message and link.

--- src/module/page/login/Component.js
+++ src/module/page/login/Component.js
@@ -12,11 +12,11 @@ export default class extends StandardPage {
         <div className="p_login ebp-wrap" >
           <Col span={24} style={{ marginTop: '30px', marginBottom: '30px' }}>
             {(this.props.loginMetamask) && <div>
-              <Spin tip="Log in to MetaMask Loading...">
+              <Spin tip="Logging in with MetaMask...">
               </Spin>
             </div>}
             {(!this.props.loginMetamask) && <div className="login-metamask">
-              <img src="/assets/images/metamask.svg" with="100px" height="100px" /><h2>Login Metamask to sign in or login with Private Key</h2>
+              <img src="/assets/images/metamask.svg" with="100px" height="100px" /><h2>Login with <a href="https://metamask.io/" target="_blank">MetaMask</a> or Private Key</h2>
             </div>}
              <LoginForm />
           </Col>
