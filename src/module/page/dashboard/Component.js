12487c9182359f97eb3569dad080d08fbaaae7b1
Zergity <zergity@gmail.com>
2019-03-26 17:00:37 +0700

    update dashboard components for consistent layout

--- src/module/page/dashboard/Component.js
+++ src/module/page/dashboard/Component.js
@@ -23,50 +23,54 @@ export default class extends LoggedInPage {
 
   getStatus (status) {
     switch (status) {
-      case 0: return 'PENDING_ACTIVE'
-      case 1: return 'ACTIVE'
-      case 2: return 'PENDING_WITHDRAW'
-      case 3: return 'WITHDRAWN'
-      case 127: return 'PENALIZED'
-      default: return 'UNKNOWN'
+      case 0: return 'Pending Active'
+      case 1: return 'Active'
+      case 2: return 'Pending Windraw'
+      case 3: return 'Withdrawn'
+      case 127: return 'Penalized'
+      default: return 'Unknown'
     }
   }
 
   ord_renderContent () { // eslint-disable-line
     return (
-      <div className="p_Profile">
+      <div className="">
         <div className="ebp-header-divider">
-
         </div>
-        <div className="ebp-page content-center">
-          <Row>
-            <Col span={12} style={{ 'display': 'block' }}>
-              <span className="text-stat">Holding</span>
-              <h1>{parseFloat(this.props.tokenBalance)/1e18} NTF </h1>
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={0} xl={0}/>
-            <Col span={12}>
-              <span className="text-stat">Deposited</span>
-              <h1>{parseFloat(this.props.depositedBalance)/1e18} NTF</h1>
-            </Col>
-          </Row>
 
-          <Row>
-            <Col span={12} style={{ 'display': 'block' }}>
-              <span className="text-stat">Status</span>
-              <h1>{this.getStatus(this.props.managerStatus)}</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <span className="text-stat">Coinbase</span>
-              <h4>{this.state.coinbase === '0x0000000000000000000000000000000000000000' ? 'Not set' : this.props.coinbase}</h4>
-            </Col>
-          </Row>
-          <div className="ebp-header-divider dashboard-rate-margin">
-
+        <div className="ebp-page">
+          <h3 className="text-center">Dashboard</h3>
+          <div className="ant-col-md-18 ant-col-md-offset-3 text-alert" style={{ 'textAlign': 'left' }}>
+            <Row>
+              <Col span={6}>
+                Holding:
+              </Col>
+              <Col xs={24} sm={24} md={24} lg={0} xl={0}/>
+              <Col span={18}>
+                {parseFloat(this.props.tokenBalance)/1e18} NTF
+              </Col>
+            </Row>
+            <Row style={{ 'marginTop': '15px' }}>
+              <Col span={6}>
+                Deposited:
+              </Col>
+              <Col xs={24} sm={24} md={24} lg={0} xl={0}/>
+              <Col span={18}>
+                {parseFloat(this.props.depositedBalance)/1e18} NTF
+              </Col>
+            </Row>
+            <Row style={{ 'marginTop': '15px' }}>
+              <Col span={6}>
+                Status:
+              </Col>
+              <Col xs={24} sm={24} md={24} lg={0} xl={0}/>
+              <Col span={18}>
+                {this.getStatus(this.props.managerStatus)}
+              </Col>
+            </Row>
+            <div className="ebp-header-divider dashboard-rate-margin">
+            </div>
           </div>
-
         </div>
       </div>
     )
