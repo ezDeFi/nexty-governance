a90d430587b0ff5f76b569267d7e8df9fef1d0b9
ThangNQ87 <bestboyvn1987@gmail.com>
2019-06-18 17:46:28 +0700

    remove test mining button

--- src/module/page/usercontrol/Component.js
+++ src/module/page/usercontrol/Component.js
@@ -328,7 +328,7 @@ export default class extends StandardPage {
                   <Col md={16} xs={24}>
                     <div className="">
                       <Button onClick={this.requestOut.bind(this)} type="ebp">Withdraw Request</Button>&nbsp;
-                      <Button onClick={this.virtuellMining.bind(this)} type="ebp">Mining(virtuell) 3ETH</Button>
+                      {/* <Button onClick={this.virtuellMining.bind(this)} type="ebp">Mining(virtuell) 3ETH</Button> */}
                     </div>
                   </Col>
                 </Row>
